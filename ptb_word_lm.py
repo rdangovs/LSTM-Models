3434ce1b535f98cd7fe24be7bfed2beb249388ed
Li Jing <jingli9111@gmail.com>
2017-08-02 18:02:38 -0700

    path and ignore

--- ptb_word_lm.py
+++ ptb_word_lm.py
@@ -77,9 +77,9 @@ flags.DEFINE_string(
 flags.DEFINE_string(
     "model", "small",
     "A type of model. Possible options are: small, medium, large.")
-flags.DEFINE_string("data_path", "/home/darumen/Research/Models/simple-examples/data",
+flags.DEFINE_string("data_path", "./data/",
                     "Where the training/test data is stored.")
-flags.DEFINE_string("save_path", "/home/darumen/Research/Models/output/PTB/",
+flags.DEFINE_string("save_path", "./PTB/",
                     "Model output directory.")
 flags.DEFINE_bool("use_fp16", False,
                   "Train using 16-bit floats instead of 32bit floats")
